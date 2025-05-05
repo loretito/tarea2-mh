cd71457eefaba8495677c82b3477f872c044b82b
sofiaignaciab <sofi.belmar9@gmail.com>
2025-05-04 20:18:20 -0400

    feat: :sparkles: add GRASP deterministic algorithm implementation for airplane landing scheduling

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from readFile import readFile
 from greedy.deterministic import greedy_deterministic
 from greedy.stochastic import greedy_stochastic
+from grasp.stochastic_hc_restart import grasp_hc_restart
 import time
+from grasp.deterministic_hc import grasp_deterministic_hc
 
 cases = readFile("cases/case1.txt")
 
@@ -28,3 +30,9 @@ for i, caso in enumerate(cases):
 
     mejor_run = min(resultados, key=lambda x: x[2])
     print(f"\nMejor Caso: Ejecución {mejor_run[0]} con costo = {mejor_run[2]:.1f}")
+
+for case in cases:
+    print("=== GRASP DETERMINISTA ===")
+    order, cost = grasp_deterministic_hc(case, num_runways=2, max_iter=1)
+    print("Solución final:", order)
+    print("Coste final:", cost)
