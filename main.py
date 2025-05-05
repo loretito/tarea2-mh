1138ffa9cb73ac790696a116582ade3a85e1ab4b
sofiaignaciab <sofi.belmar9@gmail.com>
2025-05-04 22:14:22 -0400

    feat: :sparkles: update GRASP deterministic algorithm and add stochastic GRASP with restart

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from readFile import readFile
 from greedy.deterministic import greedy_deterministic
 from greedy.stochastic import greedy_stochastic
-from grasp.stochastic_hc_restart import grasp_hc_restart
 import time
 from grasp.deterministic_hc import grasp_deterministic_hc
+from grasp.stochastic_hc import grasp_stochastic_hc_restart
 
 cases = readFile("cases/case1.txt")
 
@@ -33,6 +33,19 @@ for i, caso in enumerate(cases):
 
 for case in cases:
     print("=== GRASP DETERMINISTA ===")
-    order, cost = grasp_deterministic_hc(case, num_runways=2, max_iter=1)
+    order, cost = grasp_deterministic_hc(case, num_runways=2, max_iter=4)
+    print("Solución final:", order)
+    print("Coste final:", cost)
+
+print("=== GRASP ESTOCÁSTICO + HC (mejor mejora) + RESTART ===")
+for idx, case in enumerate(cases, start=1):
+    print(f"\n=== CASO {idx} ===")
+    order, cost = grasp_stochastic_hc_restart(
+        case,
+        alpha=0.3,
+        num_runways=2,
+        max_iter=10,
+        max_restarts=3
+    )
     print("Solución final:", order)
     print("Coste final:", cost)
